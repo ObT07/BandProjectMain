8ac678b55d7623eb95e3a5f5231d1bc6ab7d7bff
Obafemi Tidjani <132169970+ObT07@users.noreply.github.com>
2024-02-17 11:46:25 -0800

    added stuff to get  api - env and client stuff

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,7 @@
 import json
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -9,4 +11,12 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    load_dotenv()
+
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")    
+
+    print(client_id, client_secret)
+
+
     app.run(debug=True, host='0.0.0.0')
